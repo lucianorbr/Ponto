6d06741d128fabf330c76bbcb6761b00322b0ddf
Luciano <seu_email@email.com>
2020-04-11 18:06:17 -0300

    Primeiro commit, batida automatica de pontos

---
+++ index.js
@@ -0,0 +1,16 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+    const browser = await puppeteer.launch({
+        headless: false
+    });
+    const page = await browser.newPage();
+    await page.goto('http://portaldoconsultor.altran.com.br/login');
+    await page.waitFor('input[name="user_session[email]"]');
+    await page.type('input[name="user_session[email]"]', 'login', {delay: 100});
+    await page.type('input[name="user_session[password]"]', 'senha', {delay: 100});
+    await page.keyboard.press('Enter', {delay: 200});
+
+    await browser.close()
+
+})();
