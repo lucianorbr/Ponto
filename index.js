56c20b314cc453a143a7fbc3e940a45f1dc6bd43
Luciano Ribeiro <luciano.ribeiro@outlook.com>
2020-04-13 20:42:02 -0300

    configuração de job automatico e diário.

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 const puppeteer = require('puppeteer');
+const CronJob = require('cron').CronJob
+const job = new CronJob('* * * * *', () => {
 
-(async () => {
-    const browser = await puppeteer.launch({
-        headless: false
-    });
-    const page = await browser.newPage();
-    await page.goto('http://portaldoconsultor.altran.com.br/login');
-    await page.waitFor('input[name="user_session[email]"]');
-    await page.type('input[name="user_session[email]"]', 'login', {delay: 100});
-    await page.type('input[name="user_session[password]"]', 'senha', {delay: 100});
-    await page.keyboard.press('Enter', {delay: 200});
 
-    await browser.close()
+    (async () => {
+        const browser = await puppeteer.launch({
+            headless: false
+        });
+        const page = await browser.newPage();
+        await page.goto('http://portaldoconsultor.altran.com.br/login');
+        await page.waitFor('input[name="user_session[email]"]');
+        await page.type('input[name="user_session[email]"]', 'login', {delay: 100});
+        await page.type('input[name="user_session[password]"]', 'senha', {delay: 100});
+        await page.keyboard.press('Enter', {delay: 200});
 
-})();
+        await browser.close()
+
+    })();
+},null, true, 'America/Sao_Paulo')
