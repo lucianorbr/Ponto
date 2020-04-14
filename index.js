9b06b29ae1e87efb40e12a654c11d19d2c393461
Luciano Ribeiro <luciano.ribeiro@outlook.com>
2020-04-13 21:10:32 -0300

    configuração de job automatico e diário (parte III).

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const puppeteer = require('puppeteer');
+const user = require("./user.json");
+
 const CronJob = require('cron').CronJob
+
 const job = new CronJob('* * * * *', () => {
 
     (async () => {
@@ -9,8 +12,8 @@ const job = new CronJob('* * * * *', () => {
         const page = await browser.newPage();
         await page.goto('http://portaldoconsultor.altran.com.br/login');
         await page.waitFor('input[name="user_session[email]"]');
-        await page.type('input[name="user_session[email]"]', 'login', {delay: 100});
-        await page.type('input[name="user_session[password]"]', 'senha', {delay: 100});
+        await page.type('input[name="user_session[email]"]', ('user.login'), {delay: 100});
+        await page.type('input[name="user_session[password]"]', ('user.password'), {delay: 100});
         await page.keyboard.press('Enter', {delay: 200});
 
         await browser.close({delay: 500})
