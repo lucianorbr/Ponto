9157a58132ca21df91f1cc9923924aede25df283
Luciano Ribeiro <luciano.ribeiro@outlook.com>
2020-04-16 16:01:34 -0300

    Configurando os pontos

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 
 const CronJob = require('cron').CronJob
 
-const job = new CronJob('* * * * *', () => {
+const entrada = new CronJob('56 12 * * *', () => {
 
     (async () => {
         const browser = await puppeteer.launch({
@@ -13,11 +13,66 @@ const job = new CronJob('* * * * *', () => {
         const page = await browser.newPage();
         await page.goto('http://portaldoconsultor.altran.com.br/login');
         await page.waitFor('input[name="user_session[email]"]');
-        await page.type('input[name="user_session[email]"]', ('user.login'), {delay: 100});
-        await page.type('input[name="user_session[password]"]', ('user.password'), {delay: 100});
+        await page.type('input[name="user_session[email]"]', user.login, {delay: 100});
+        await page.type('input[name="user_session[password]"]', user.password, {delay: 100});
         await page.keyboard.press('Enter', {delay: 200});
 
-        await browser.close({delay: 500})
+        //await browser.close({delay: 500})
 
     })();
 },null, true, 'America/Sao_Paulo')
+
+const saidaalmoco = new CronJob('57 12 * * *', () => {
+
+    (async () => {
+        const browser = await puppeteer.launch({
+            headless: false
+        });
+        const page = await browser.newPage();
+        await page.goto('http://portaldoconsultor.altran.com.br/login');
+        await page.waitFor('input[name="user_session[email]"]');
+        await page.type('input[name="user_session[email]"]', user.login, {delay: 100});
+        await page.type('input[name="user_session[password]"]', user.password, {delay: 100});
+        await page.keyboard.press('Enter', {delay: 200});
+
+        //await browser.close({delay: 500})
+
+    })();  
+},null, true, 'America/Sao_Paulo')
+
+const voltaalmoco = new CronJob('58 12 * * *', () => {
+
+    (async () => {
+        const browser = await puppeteer.launch({
+            headless: false
+        });
+        const page = await browser.newPage();
+        await page.goto('http://portaldoconsultor.altran.com.br/login');
+        await page.waitFor('input[name="user_session[email]"]');
+        await page.type('input[name="user_session[email]"]', user.login, {delay: 100});
+        await page.type('input[name="user_session[password]"]', user.password, {delay: 100});
+        await page.keyboard.press('Enter', {delay: 200});
+
+        //await browser.close({delay: 500})
+
+    })();  
+},null, true, 'America/Sao_Paulo')
+
+const finaldia = new CronJob('59 12 * * *', () => {
+
+    (async () => {
+        const browser = await puppeteer.launch({
+            headless: false
+        });
+        const page = await browser.newPage();
+        await page.goto('http://portaldoconsultor.altran.com.br/login');
+        await page.waitFor('input[name="user_session[email]"]');
+        await page.type('input[name="user_session[email]"]', user.login, {delay: 100});
+        await page.type('input[name="user_session[password]"]', user.password, {delay: 100});
+        await page.keyboard.press('Enter', {delay: 200});
+
+        //await browser.close({delay: 500})
+
+    })();  
+},null, true, 'America/Sao_Paulo')
+
